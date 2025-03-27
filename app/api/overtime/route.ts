da8557d69040a2190831c44b072494b2312f67b9
Saman Tripathee <tripatheesaman@gmail.com>
2025-03-27 18:55:45 +0545

    Added logic for the night overtime processing for the initital time

--- app/api/overtime/route.ts
+++ app/api/overtime/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { FormData } from "@/app/types/InputFormType";
 import processRawTime from "@/app/lib/helpers/timetransformation";
+import FormatNightTime from "@/app/lib/helpers/formatNightTime";
 
 export const POST = async (req: NextRequest) => {
+  let hasNightDays = false
   if (!(req.method === "POST"))
     return NextResponse.json({ error: "Method not allowed" }, { status: 401 });
 
@@ -12,14 +14,32 @@ export const POST = async (req: NextRequest) => {
       { status: 400 }
     );
   const data: FormData = (await req.json()) as FormData;
-  console.log("Data", data);
   if (!data || typeof data !== "object")
     return NextResponse.json(
-      { error: "invalid data received !" },
+      { error: "Invalid data received !" },
       { status: 400 }
     );
   const processedAttendanceData = processRawTime(data.inOutTimes);
+  if (!processedAttendanceData || !(processedAttendanceData.length > 0)){
+    return NextResponse.json(
+      {error:"Invalid data after processing !"},
+      {status:400}
+    )
+  }
+  if (!data.nightDutyDays){
+        return NextResponse.json(
+          { error: "Invalid night data received!" },
+          { status: 400 }
+        );
+  }
+  if (data.nightDutyDays.length>0 && data.nightDutyDays){
+    hasNightDays = true
+    const attendanceDataAfterNightDuty = FormatNightTime(processedAttendanceData, data.nightDutyDays)
+  }
+  // const nightTimeFormattedAttendanceData = FormatNightTime(processedAttendanceData, )
+    
   if (processedAttendanceData.length < 1)
     return NextResponse
       .json({ error: "Invalid data found after processing !" }, {status:400});
+  
 };
