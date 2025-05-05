2a916aea77208c780f08ec616932b6c20b27b34e
Saman Tripathee <tripatheesaman1@gmail.com>
2025-05-05 21:59:45 +0545

    Fixed night time data processing logic and implemented level 1

--- app/lib/helpers/formatNightTime.ts
+++ app/lib/helpers/formatNightTime.ts
@@ -4,30 +4,7 @@ const FormatNightTime = (
   attendanceData: AttendanceRecord[],
   nightDutyDays: number[]
 ) => {
-  const sortedNightDays = [...nightDutyDays].sort((a, b) => a - b);
-  if (JSON.stringify(sortedNightDays) !== JSON.stringify(nightDutyDays)) {
-    return attendanceData;
-  }
-  for (let i = 0; i < nightDutyDays.length - 1; i++) {
-    const currentIndex = nightDutyDays[i] - 1;
-    const nextIndex = nightDutyDays[i + 1] - 1;
-
-    // First day's inTime remains unchanged
-    if (i === 0) {
-      attendanceData[currentIndex].outTime = attendanceData[nextIndex].inTime;
-    } else {
-      attendanceData[currentIndex].inTime =
-        attendanceData[currentIndex].outTime;
-      attendanceData[currentIndex].outTime = attendanceData[nextIndex].inTime;
-    }
-  }
-
-  // Last day's inTime becomes its outTime
-  const lastIndex = nightDutyDays[nightDutyDays.length - 1] - 1;
-  attendanceData[lastIndex].inTime = attendanceData[lastIndex].outTime;
-  attendanceData[lastIndex].outTime = attendanceData[lastIndex + 1].inTime;
-  attendanceData[lastIndex + 1].inTime = "NA";
-
+  // No longer need to swap times, just return the data as is
   return attendanceData;
 };
 
